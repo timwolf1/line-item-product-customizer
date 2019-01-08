93b63c26216064f4fc2b8f2e66ea27da0e73725c
Tim Wolf <timwolf1@gmail.com>
2019-01-08 00:56:38 -0500

    Added basic add customization box functionality

--- js/main.js
+++ js/main.js
@@ -3,8 +3,12 @@ const domElements = {
     engraveQtyContainer: "qtyInputContainer",
     engraveQtyInput: "engraveQtyInput",
     engraveQtyBtn: "engraveQtyConfirm",
-    itemCustomizerContainer: "customizationBox-container"
+    itemCustomizerContainer: "customizationBox-container",
+    itemCustomizerSpan: "customizationBox-span",
+    addCustBoxLink: "addCustBoxLink"
 };
+
+let numOfItems = 0;
 
 document.getElementById(domElements.engravePromptCheckbox).addEventListener("change", event => {
     if (event.target.checked) {
@@ -17,3 +21,22 @@ document.getElementById(domElements.engravePromptCheckbox).addEventListener("cha
 document.getElementById(domElements.engraveQtyBtn).addEventListener("click", event => {
     console.log(event);
 });
+
+document.getElementById(domElements.addCustBoxLink).addEventListener("click", event => {
+    event.preventDefault();
+    numOfItems++;
+    addCustomizationBox(numOfItems);
+});
+
+const addCustomizationBox = (num) => {
+    const markup = `
+        <div class="customizationBox-item" id="customizationBox-item-${num}">
+            <p style="text-align: center">ITEM ${num}</p>
+            <input type="text" placeholder="LINE 1" size="16" style="margin: 10px"><input type="checkbox">
+            <input type="text" placeholder="LINE 2" size="16" style="margin: 10px"><input type="checkbox">
+            <input type="text" placeholder="LINE 3" size="16" style="margin: 10px"><input type="checkbox">
+        </div>
+    `;
+
+    document.getElementById(domElements.itemCustomizerSpan).insertAdjacentHTML("beforeend", markup);
+};
